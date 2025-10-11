34ea5acd7e584f4d0f283ad85d7d5c2c303a8c95
carovine <carvin2105@gmail.com>
2025-10-11 11:57:17 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar.tsx'
+import Recipes from './pages/Recipes.tsx'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Recipes />
+    </>
+  )
+}
+
+export default App
