83ea0d052379bfce2e105c8b3816ebfb52e1aab9
carovine <carvin2105@gmail.com>
2025-10-27 17:58:41 +0530

    feat: add environment configuration and update server setup
    - Added new dependencies: @neondatabase/serverless, cron, drizzle-orm, and drizzle-kit.
    - Created a new config file (env.js) to manage environment variables.
    - Updated server.js to use the new ENV configuration for the PORT variable.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { ENV } from './config/env.js';
+
 
 dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Recipe Finder API is running');
